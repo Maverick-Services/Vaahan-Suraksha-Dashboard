5894171d64085078855897dfa354024dc0b4fe02
Maverick-Services <maverickit123@gmail.com>
2025-08-14 14:45:59 +0530

    skeleton update

--- src/components/shared/TableSkeleton.jsx
+++ src/components/shared/TableSkeleton.jsx
@@ -5,47 +5,46 @@ import {
     TableHead, TableRow, Paper, Skeleton
 } from "@mui/material";
 
-export default function TableSkeleton({ rows = 5 }) {
+export default function TableSkeleton({ rows = 5, columns = 5 }) {
     return (
         <Paper sx={{ width: "100%", overflow: "hidden" }}>
             <TableContainer>
                 <Table stickyHeader>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Sr No</TableCell>
-                            <TableCell>User ID</TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Phone No</TableCell>
-                            <TableCell>Email ID</TableCell>
-                            <TableCell>Joining Date</TableCell>
-                            <TableCell align="right">Actions</TableCell>
+                            {[...Array(columns)].map((_, idx) => (
+                                <TableCell key={idx}>
+                                    <Skeleton
+                                        variant="text"
+                                        width="80%"
+                                        sx={{ bgcolor: "grey.400" }}
+                                    />
+                                </TableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
 
                     <TableBody>
                         {[...Array(rows)].map((_, i) => (
                             <TableRow key={i}>
-                                <TableCell>
-                                    <Skeleton variant="text" width={20} />
-                                </TableCell>
-                                <TableCell>
-                                    <Skeleton variant="text" width={80} />
-                                </TableCell>
-                                <TableCell>
-                                    <Skeleton variant="text" width={120} />
-                                </TableCell>
-                                <TableCell>
-                                    <Skeleton variant="text" width={100} />
-                                </TableCell>
-                                <TableCell>
-                                    <Skeleton variant="text" width={160} />
-                                </TableCell>
-                                <TableCell>
-                                    <Skeleton variant="text" width={100} />
-                                </TableCell>
-                                <TableCell align="right">
-                                    <Skeleton variant="circular" width={24} height={24} />
-                                </TableCell>
+                                {[...Array(columns)].map((_, j) => (
+                                    <TableCell key={j}>
+                                        {j === columns - 1 ? (
+                                            <Skeleton
+                                                variant="rounded"
+                                                width={64}
+                                                height={24}
+                                                sx={{ bgcolor: "grey.400" }}
+                                            />
+                                        ) : (
+                                            <Skeleton
+                                                variant="text"
+                                                width="80%"
+                                                sx={{ bgcolor: "grey.400" }}
+                                            />
+                                        )}
+                                    </TableCell>
+                                ))}
                             </TableRow>
                         ))}
                     </TableBody>
