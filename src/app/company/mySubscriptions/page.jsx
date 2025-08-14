75143476e27b1c20054de5971d3dbeab7d908a3f
Maverick-Services <maverickit123@gmail.com>
2025-08-14 12:58:59 +0530

    updates

---
+++ src/app/company/mySubscriptions/page.jsx
@@ -0,0 +1,13 @@
+import InnerDashboardLayout from '@/components/dashboard/InnerDashboardLayout'
+import PageHeading from '@/components/shared/PageHeading'
+import React from 'react'
+
+function page() {
+    return (
+        <InnerDashboardLayout>
+            <PageHeading>My Subscriptions</PageHeading>
+        </InnerDashboardLayout>
+    )
+}
+
+export default page
---
+++ src/app/company/profile/page.jsx
@@ -0,0 +1,13 @@
+import InnerDashboardLayout from '@/components/dashboard/InnerDashboardLayout'
+import PageHeading from '@/components/shared/PageHeading'
+import React from 'react'
+
+function page() {
+    return (
+        <InnerDashboardLayout>
+            <PageHeading>My Profile</PageHeading>
+        </InnerDashboardLayout>
+    )
+}
+
+export default page
